13fac840e10bd1f9a1aad9b1e3d9f64da20c96fb
HAUDRAUFHAUN <47633893+HAUDRAUFHAUN@users.noreply.github.com>
2020-04-18 21:43:07 +0200

    Adding Honey Tab to URL-List

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/honey')
+def honey():
+    return render_template('honey.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
